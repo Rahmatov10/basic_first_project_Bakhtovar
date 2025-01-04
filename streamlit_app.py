e71b57a307e73064522bfe08026e11d09b2ea145
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-04 19:09:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,9 @@ st.write('Hello bakhtovar!')
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
 with st.expander("Data"):
   st.write("X")
+  X_raw = df.drop("species", axis=1)
+  st.dataframe(X_raw)
+
+  st.write("y")
+  y_raw = df.drop("species", axis=1)
+  st.dataframe(y_raw)
