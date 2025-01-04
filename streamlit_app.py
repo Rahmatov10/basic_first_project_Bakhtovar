04065cc2c0449472195c728106b455aaa7b4527c
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-04 19:33:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,40 @@ with st.sidebar:
 
   body_mass_g = st.slider('Body mass (g)', 32.1, 59.6, 44.5)
   island = st.selectbox('Gender', ('female', 'male'))
+
+st.subheader('Data Visualization')
+
+fig = px.scatter(
+
+    df,
+
+    x='bill_length_mm',
+
+    y='bill_depth_mm',
+
+    color='island',
+
+    title='Bill Length vs. Bill Depth by Island'
+
+)
+
+st.plotly_chart(fig)
+
+ 
+
+fig2 = px.histogram(
+
+    df, 
+
+    x='body_mass_g', 
+
+    nbins=30, 
+
+    title='Distribution of Body Mass'
+
+)
+
+st.plotly_chart(fig2)
+
+
+
