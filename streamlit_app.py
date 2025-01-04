c0fd8c0d75ce7fa6a25cab226921152f68f8c5ea
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-04 19:05:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-
+import pandas as pd
 st.title('My first basic project')
 
 st.write('Hello bakhtovar!')
 
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+with st.expander("Data"):
+  st.write("X")
