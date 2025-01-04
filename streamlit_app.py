3a635f163602f6ca87684442f856a518d8f667c2
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-04 19:04:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title('My first basic project')
 
 st.write('Hello bakhtovar!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
